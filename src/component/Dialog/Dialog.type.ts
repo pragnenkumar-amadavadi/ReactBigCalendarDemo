9c107088f0dc080d75518aacb4d536fd42cc3e1e
Pragnenkumar Amadavadi <pragnenkumar.amadavadi@smartsensesolutions.com>
2025-12-05 12:26:18 +0530

    Remove Dialog and TimePicker components along with their associated styles and stories to streamline the codebase.

--- src/component/Dialog/Dialog.type.ts
+++
@@ -1,7 +0,0 @@
-import { type DialogProps as MuiDialogProps } from "@mui/material/Dialog";
-
-export interface CustomDialogProps extends MuiDialogProps {
-  title?: string;
-  onClose: () => void;
-  subtitle?: string;
-}
--- src/component/TimePicker/TimePicker.type.ts
+++
@@ -1,5 +0,0 @@
-import { type TimePickerProps as MuiTimePickerProps } from "@mui/x-date-pickers/TimePicker";
-
-export interface CustomTimePickerProps extends MuiTimePickerProps {
-  label?: string;
-}
